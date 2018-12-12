29696f7c02244ae8557fc8c88c662cbc385ebf9e
yunmingyang <hjkl0809@163.com>
2018-12-12 17:52:49 +0800

    rm

--- src/main/java/oldcode/Pet/Individual.java
+++
@@ -1,8 +0,0 @@
-package com.Pet;
-
-public class Individual {
-    Individual(String name){ System.out.println(name); };
-    Individual(){
-        System.out.println("无参构造");
-    }
-}
--- src/main/java/oldcode/Pet/Person.java
+++
@@ -1,5 +0,0 @@
-package com.Pet;
-
-public class Person extends Individual{
-    public Person(String name){ super(name); }
-}

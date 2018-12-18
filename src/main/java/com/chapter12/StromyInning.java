fd3151f90d06ba929ad30a98e231c8b45eaccc67
yunmingyang <hjkl0809@163.com>
2018-12-18 21:19:55 +0800

    modify

--- src/main/java/com/chapter12/StromyInning.java
+++ src/main/java/com/chapter12/StromyInning.java
@@ -24,12 +24,14 @@ interface Storm{
     public void rainHard() throws RainedOut;
 }
 public class StromyInning extends Inning implements Storm {
-    public StromyInning()throws RainedOut,BaseballException {}
-    public StromyInning(String s)throws RainedOut,BaseballException{}
+    public StromyInning()throws RainedOut,BaseballException {}//由于需要调用父类构造所以此处需要将父类抛出的异常写在异常列表中
+    public StromyInning(String s)throws Foul,BaseballException{}
+//    void walk(){}//访问修饰符需要大于父类中函数定义处的访问修饰符
 //    public void walk() throws Foul{}//基类无异常抛出，所以此处会报错
-//导出类可以不抛出异常，或者基类中没有的方法可以抛出任意异常，否则就应该抛出基类中该方法定义好的异常
-    public void atBat(){ }
+    public void atBat() throws PopFoul{}//
     public void rainHard(){}
+//    书中原话---接口不能对base类中已存在的方法添加异常，那么此处方法其实对base类中相应方法的重写，所以此处要么不抛异常，要么抛出base相同或者派生异常
+//    public void event() throws RainedOut{}
     public void event(){}
 
     public static void main(String[] args) {
@@ -46,7 +48,6 @@ public class StromyInning extends Inning implements Storm {
             System.out.println("Generic baseball exception");
         }
 //若使用多态，则需要捕获其基类的异常
-//且由于异常不属于方法的一部分，所以不可以用异常来重载方法
         try{
             Inning i = new StromyInning();
         }catch(Strike e){

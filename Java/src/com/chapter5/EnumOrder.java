6fc4d1dbe386dd8c21b22922637261ab175534ab
yunmingyang <hjkl0809@163.com>
2019-07-15 00:26:51 +0800

    add cases

--- Java/src/com/chapter5/EnumOrder.java
+++ Java/src/com/chapter5/EnumOrder.java
@@ -8,15 +8,23 @@ public class EnumOrder {
         Spiciness sn = Spiciness.HOT;
         System.out.println(sn);
 
-        for (Spiciness s : Spiciness.values())
+        for (Spiciness s : Spiciness.values()) {
             System.out.println(s + ", ordinal " + s.ordinal());
+            System.out.println(s + ", code " + s.code);
+        }
         Burrito b = new Burrito(Spiciness.MILD);
         b.describe();
     }
 }
 
 enum Spiciness {
-    NOT, MILD, MEDIUM, HOT, FLAMING
+    NOT(10), MILD(20), MEDIUM(30), HOT(40), FLAMING(50);
+
+    // 其它域和方法的定义要在定义了枚举元素之后
+    int code;
+    Spiciness(int code){this.code = code;}
+
+    int getCode(){return this.code;}
 }
 
 class Burrito {
